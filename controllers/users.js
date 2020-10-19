f3c757d64950cc1fd01622ca4d0e674b7ca0c664
Aleksey Efimov <factorng@yandex.ru>
2020-10-19 17:55:30 +0300

    prj15/first attempt/fix4

--- controllers/users.js
+++ controllers/users.js
@@ -56,7 +56,7 @@ const login = (req, res, next) => {
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
           httpOnly: true,
-          sameSite: true,
+          sameSite: false,
         })
         .send({ token });
     })
